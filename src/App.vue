5a11bc797e5cbd39f11a691c959d839bb396aea0
HR <56949560+HR-2019@users.noreply.github.com>
2021-06-18 21:04:05 -0500

    Inicio

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer 
+     v-model="drawer" 
+     :clipped="$vuetify.breakpoint.lgAndUp"
+     app  
+    >
+    <v-list dense>
+      <template>
+        <v-list-item :to="{ name:'Home' }">
+          <v-list-item-action>
+            <v-icon>home</v-icon>
+          </v-list-item-action>
+          <v-list-item-title>
+            Inicio
+          </v-list-item-title>
+        </v-list-item>
+      </template>
+    </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar 
+      :clipped-left="$vuetify.breakpoint.lgAndUp" 
+      app
+      color="blue darken-3" 
+      dark
+    >
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+        <span class="hidden-sm-and-down">HeartHomeUI</span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>view_list</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container 
+        class="fill-height" 
+        fluid
+      >
+        <v-slide-x-transition mode="out-in">
+          <router-view />
+        </v-slide-x-transition>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    drawer:null
+  }),
+};
+</script>
